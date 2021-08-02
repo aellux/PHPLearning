70113716aec752519805890e24cc6700ba37e8c6
aellux <alexluc2001@gmail.com>
2021-08-02 00:05:48 -0700

    Add files via upload

--- flamecalc.php
+++ flamecalc.php
@@ -6,20 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flaming Calculator</title>
 </head>
+Flaming Calculator
 <body>
-    <form action = "flamecalc.php" method = "post">
-       Thief: <input type="checkbox" name = "expClass[]" value="thief"><br>
-       Mage: <input type="checkbox" name = "expClass[]" value="mage"><br>
-       Pirate: <input type="checkbox" name = "expClass[]" value="pirate"><br>
-       Warrior: <input type="checkbox" name = "expClass[]" value="warrior"><br>
-       Archer: <input type="checkbox" name = "expClass[]" value="archer"><br>
-        <input type = "submit">
+    <form action = "flamecalc.php" method = "POST">
+       Thief: <input type="checkbox" name = "expClass[]" value="Thief"><br>
+       Mage: <input type="checkbox" name = "expClass[]" value="Mage"><br>
+       Pirate: <input type="checkbox" name = "expClass[]" value="Pirate"><br>
+       Warrior: <input type="checkbox" name = "expClass[]" value="Warrior"><br>
+       Archer: <input type="checkbox" name = "expClass[]" value="Archer"><br>
+        
+Input the flame score <br>
+STR: <input type="number" name = "baseStr" value = "0"><br>
+DEX: <input type="number" name = "baseDex" value = "0"><br>
+INT: <input type="number" name = "baseInt" value = "0"><br>
+LUK: <input type="number" name = "baseLuk" value = "0"><br>
+ATK: <input type="number" name = "baseAtk" value = "0"><br>
+MATT: <input type="number" name = "baseMatk" value = "0"><br>
+ALL %: <input type="number" name = "allStat" value = "0"><br>
+<input type = "submit"><br>
+Flame Score:
 </form>
     <?php
     $expClass = $_POST["expClass"];
     echo $expClass[0];
-    $allStat = 8;
-    $baseStat;
+    function calcFlame()
+    {
+    if (isset($_POST['Thief']))
+        {
+        echo $_POST['baseStr'] + ($_POST['allStat'] * 8);
+        }
+    }
+
     ?>
 </body>
 </html>
